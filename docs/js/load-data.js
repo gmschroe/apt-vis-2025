ab9c72848096a7b9d715620c20369c7dd43a477e
Gabrielle Schroeder <8205451+gmschroe@users.noreply.github.com>
2024-11-27 13:22:09 +0000

    path update

--- docs/js/load-data.js
+++ docs/js/load-data.js
@@ -1,11 +1,11 @@
 // Levels separated into separate indicators, for time series plot
-d3.csv("/data/data_apt_ts_separated.csv", d3.autoType).then(data => {
+d3.csv("data/data_apt_ts_separated.csv", d3.autoType).then(data => {
     console.log("ts data", data.slice(0, 50));
     drawStackedTimeSeries(data);
     createFilters(data);
 })
 
 // Levels encoded as different values, for radial plots
-d3.csv("/data/data_apt_ts.csv", d3.autoType).then(data => {
+d3.csv("data/data_apt_ts.csv", d3.autoType).then(data => {
     console.log("radial data", data.slice(0, 50));
 })
