8ec321e48413e266e5eef0d5c7c93a71f9330ab8
Gabrielle Schroeder <8205451+gmschroe@users.noreply.github.com>
2025-03-28 14:10:42 +0000

    comment updates

--- R/update_data_for_visualisations.R
+++ R/update_data_for_visualisations.R
@@ -1,6 +1,10 @@
 # Script to run the data calculations and formatting steps on a new version of 
-# the APT indicator data.
-# Run from within the "vfsg-apt/R/" folder
+# the APT indicator data, then save the formatted data and launch the updated
+# visualisation.
+# Your working directory must be the "vfsg-apt/R/" folder.
+#
+# Gabrielle M. Schroeder
+# March 2025
 
 # Steps to update:
 # 1) Add an xlxs file with the updated data to the "vfsg-apt/R/data/" folder. 
@@ -18,32 +22,34 @@
 #
 # This script will also launch the updated Javascript visualisation locally 
 # using a web browser so you can view the changes.
+#
+# Note that if the data validation checks fail, the script will stop and the 
+# data will NOT be updated. See repository README for example of failed check.
 
 # Prep -------------------------------------------------------------------------
 
 # Clear workspace
 rm(list = ls())
 
-# Load functions for data formatting and calculations
-# (This file also loads necessary libraries)
+# Load functions for data formatting and calculations.
+# (This file also loads necessary libraries.)
 source(file.path("lib", "data_formatting.R"))
 
 # Update this section for new data ---------------------------------------------
 
-# Name of your Excel file
-# Must be a file in the "data" folder
+# Name of your Excel file. Must be a file in the "data" folder.
 apt_data_file_name <- "APT_data-info-dictionary_final.xlsx"
 
 # Desired name of output folder - will be created inside the existing 
 # "data_exports" folder.
-# Today's date will also be added as a prefix to the folder's name
+# Today's date will also be added as a prefix to the folder's name.
 output_dir <- "original_data"
 
 # Update data and save in vfsg-apt/R/data_exports and vfsg-apt/docs/data -------
 
 # Full path to file
 
-# Create data frames with reformatted data
+# Create data frames with reformatted data.
 # start_year = the lower limit for the year axis on the charts
 # If you do NOT want to update the visualisation data yet, you can set 
 # save_to_vis_dir to FALSE (but must be TRUE to apply the new data to the 
@@ -55,17 +61,17 @@ apt_dataframes <- format_and_save_apt_data(
   save_to_vis_dir = TRUE
 )
 
-# Optional: view the reformatted data
+# Optional: view the reformatted data in RStudio
 View(apt_dataframes$data_apt_bar)
 View(apt_dataframes$data_apt_radial)
 
 # View the visualisation with the update data ----------------------------------
 # This code uses the data in vfsg-apt/docs/data - save_to_vis_dir must be set to
-# TRUE for the visualisation to reflect the updated data data files.
+# TRUE for the visualisation to reflect the updated data files.
 # The visualisation will be launched in your browser.
 # Because we're using the httw (w for "watch") function, this launched page 
-# should also update if any changes are made inside the docs folder, even if the
-# page is not re-launched.
+# should also update if any changes are made inside the docs folder during this
+# R session, even if this code is not re-run, until the page is closed.
 
 # You can also run the below code without updating the data - it will use the
 # data last saved to the vfsg-apt/docs/data folder.
