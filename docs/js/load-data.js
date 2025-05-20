62712a54e48c439eb59a967cbdd188157c5fe2d4
Gabrielle Schroeder <8205451+gmschroe@users.noreply.github.com>
2025-05-20 12:17:24 +0100

    added placeholders for translations

--- docs/js/load-data.js
+++ docs/js/load-data.js
@@ -1,13 +1,19 @@
 // Paths must start with directory in root directory (docs)
 
+let dataRoot = "../"; // for languages other than english
+switch (lang) {
+    case "eng":
+        dataRoot = "";
+        break;
+}
 // Levels separated into separate indicators, for time series bar plot
-d3.csv("data/data_apt_bar.csv", d3.autoType).then(data => {
+d3.csv(`${dataRoot}data/data_apt_bar.csv`, d3.autoType).then(data => {
     drawStackedTimeSeries(data);
     createRegionFilters(data);
 })
 
 // Levels encoded as different values, for radial plots
-d3.csv("data/data_apt_radial.csv", d3.autoType).then(data => {
+d3.csv(`${dataRoot}data/data_apt_radial.csv`, d3.autoType).then(data => {
     drawRadialPlots(data);
     createIndicatorFilters(data);
     createRadialTooltip();
