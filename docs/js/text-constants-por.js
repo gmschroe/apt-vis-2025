bf6af28a1337fb9e7eb31dc08928ead5b13a87e4
Gabrielle Schroeder <8205451+gmschroe@users.noreply.github.com>
2025-06-01 20:31:15 +0100

    file for Portuguese text (not yet translated)

---
+++ docs/js/text-constants-por.js
@@ -0,0 +1,71 @@
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+// VARIABLES FOR TEXT DEPENDENT ON LANGUAGE - ENGLISH VERSION
+//
+// TRANSLATION NOTES:
+//
+// <br> are linebreaks used to wrap text around the visualisations, and they can be 
+// added at any point (do not try to maintain exact placements in translations)
+
+// How to refer to countries/states
+const statesText = "states"
+const statesTextSingular = "state" // singular version
+
+// Indicator labels
+// Keep this order!
+const indInfoLabels = [
+  "Ratified UN Convention against Torture", 
+  "Ratified UN Optional Protocol",
+  "Submitted report to Committee against Torture",
+  "Prohibit torture in the constitution",
+  "Criminalise torture in domestic law",
+  "Have an NHRI that meets the Paris Principles",
+  "Have a National Preventive Mechanism"
+]
+
+// Text for different levels for criminalisation in domestic law and NPM
+// First is for full, second is for partial
+// Keep extra space before text
+const lawLevelsText = [
+  " (fully criminalised)",
+  " (partially criminalised)"
+]
+const npmLevelsText = [
+  " (designated and operational)",
+  " (designated only)"
+]
+
+// Time series plot --------------------------------------------------------------------
+// Visualisation text
+const textTsTitle = "A progress dashboard of a global commitment to torture prevention";
+const textTsP = "The APT has identified eight key legal measures that indicate a commitment to preventing torture. "
+  + "Each <br> bar shows the total number of states that have implemented each legal measure, "
+  + "with<br>stripes indicating partial implementations.";
+
+// Labels for regions for buttons used to filter the bar chart
+const regionFiltersLabels = [
+  "All regions",
+  "Americas",
+  "Africa",
+  "Europe",
+  "Middle East",
+  "Asia-Pacific"
+]
+
+// How regions are referred to in the bar chart legend
+const regionFiltersTextSentence = [
+  "around the world",
+  "in the Americas",
+  "in Africa",
+  "in Europe",
+  "in the Middle East",
+  "in the Asia-Pacific"
+]
+
+// Radial plot -------------------------------------------------------------------------
+// Visualisation text
+const textRadialTitle = "Continuous action towards torture prevention";
+const textRadialP = "To create a world without torture, every state needs to commit to preventing torture. "
+  + "Each bar shows when a state implemented this torture prevention measure, with stripes indicating partial implementations."
+const textRadialPHover = "Hover over the bars to see the details for each state."
